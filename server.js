0ab7675b4c23fa70928d445de1b667482fd0a4e2
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-05-11 17:36:00 -0700

    Connected AtlasDB

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-const express = require('express');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const compression = require('compression');
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/budget';
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+// const MONGODB_URI =
+//   "mongodb+srv://tvnames:INdianapolis12!!@cluster0.w8jgr.mongodb.net/Budget?retryWrites=true&w=majority";
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
 
 // routes
-app.use(require('./routes/api.js'));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
